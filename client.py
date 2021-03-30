30e62bffb8fd149f4901553330d2c1a36affd1c7
Danilo Silva <danilojose26@hotmail.com>
2021-03-30 20:15:21 -0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import socket
+import struct
+from pathlib import Path
+import sys
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+print("Sou o Cliente")
+
+with open('teste.txt', 'rb') as f: 
+    dado = f.read() 
+    
+#for i in range(len(dado)):    
+#    print(dado[i])
+
+mesageOut = []
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    for i in range(len(dado)):          
+        s.sendall(chr(dado[i]).encode('utf-8'))           
+        data = s.recv(1024)        
+        mesageOut.append(data)        
+
+print('Received: ', repr(mesageOut))
