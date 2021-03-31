0ccc6e75d29cdb23fb0e4991f4b70b2d2c9ca726
Sia2602 <danilojose26@hotmail.com>
2021-03-31 19:27:41 -0300

    Adicionando Modificacoes

--- client.py
+++ client.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 
 import socket
-import struct
-from pathlib import Path
-import sys
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 print("Sou o Cliente")
 
-file= open("arquivosTest/teste.txt", "r", encoding="utf-8") 
+file= open("arquivosTest/GRUPO6.txt", "r", encoding="utf-8") 
 dado = file.read()
 
-print(len(dado))    
-for i in range(len(dado)):    
-    print(dado[i])
+#print(len(dado))    
+#for i in range(len(dado)):    
+#    print(dado[i])
 
-mesageOut = []
+mesageOut = ""
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     for i in range(len(dado)):          
         s.sendall(bytes(dado[i], encoding="oem"))           
         data = s.recv(1024)        
-        mesageOut.append(data)        
+        mesageOut+=data.decode('oem') 
 
-print('Received: ', repr(mesageOut))
+print('Received:', mesageOut)
--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import socket
-import struct
-import codecs
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -12,12 +10,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
+    #mesageOut = ""
     with conn:
         print('Connected by', addr)
         while True:
-            data = conn.recv(1024)            
-            
-            print(data.decode('oem'))
+            data = conn.recv(1024)             
+            #mesageOut+=data.decode('oem')     
+            #print(mesageOut)  
+            #print(str.encode(mesageOut))                 
+            #print(data.decode('oem'))            
             if not data:
-                break
+                break                       
             conn.sendall(data)
